bc2e1e2de9a513f645934864583644813c4e9405
Zsolt Kébel <25386639+zsoltkebel@users.noreply.github.com>
2025-02-06 12:35:32 +0000

    Add unittest to compare GitHub implementation with my custom implementation

---
+++ dynamic_routing.py
@@ -0,0 +1,103 @@
+import logging
+
+import torch
+import torch.nn.functional as F
+
+logger = logging.getLogger(__name__)
+
+def squash(input_tensor):
+    squared_norm = (input_tensor ** 2).sum(-1, keepdim=True)
+    output_tensor = squared_norm * input_tensor / ((1. + squared_norm) * torch.sqrt(squared_norm))
+    return output_tensor
+
+# Procedure 1 Routing algorithm.
+def procedure1(in_capsules, out_capsule_count, out_capsule_dimension, r: int=3, W=None):
+    """Dynamic routing.
+    
+    Args:
+        in_capsules: the matrix of incomding capsules of shape (caps count x caps dimension)
+        out_capsule_count: number of capsules in upper level
+        out_capsule_dimension: dimension of upper level capsules
+        r: number of dynamic routing iterations (defaults to 3)
+        W: the weight matrix (defaults to None) if None it is randomly generated
+            this should have shape of (in_capsule_count, out_capsule_count, out_capsule_dimension, in_capsule_dimension)
+    """
+    in_capsule_count, in_capsule_dimension = in_capsules.size()
+
+    if W is None:
+        W = torch.randn(in_capsule_count, out_capsule_count, out_capsule_dimension, in_capsule_dimension)
+
+    # line 2: for all capsule i in layer l and capsule j in layer (l+1): bij ← 0.
+    b_ij = torch.zeros(in_capsule_count, out_capsule_count)
+
+    logger.debug("Starting dynamic routing [%d x %d] -> [%d x %d] (capsule count x capsule dimension)", in_capsule_count, in_capsule_dimension, out_capsule_count, out_capsule_dimension)
+
+    # Reshaping capsules_layer_0 to match dimensions for multiplication with weight matrix W
+    # begins with lower layer sizes
+    # [num_of_capsules, capsule_dimension]
+    # --unsqueeze(1)-->
+    # [num_of_capsules, 1, capsule_dimension]
+    # --expand(-1, num_of_capsules_layer_next, -1)-->
+    # [num_of_capsules, num_of_capsules_next_layer, capsule_dimension]
+    # --unsqueeze(-1)-->
+    # [num_of_capsules, num_of_capsules_next_layer, capsule_dimension, 1]
+    u = (
+        in_capsules
+        .unsqueeze(1) # add a new dimension at index 1
+        .expand(-1, out_capsule_count, -1)
+        .unsqueeze(-1) # add a new dimension after the current last
+    )
+    # ^ format incoming capsules into a shape that can be matrix multiplied with the weight
+    logger.debug("Incoming capsules shape after adjuting for matrix multiplication by the weight matrix: %s", u.size())
+
+    # Compute predicted outputs (u_hat) using matrix multiplication
+    u_hat = torch.matmul(W, u)  # Shape: [3, 2, 2, 1]
+    u_hat = u_hat.squeeze(-1)  # Shape: [3, 2, 2] (Remove the last singleton dimension)
+    print("u_hat:", u_hat)
+    print("u_hat size:", u_hat.size())
+
+    # line 3: for r iterations do
+    for iteration in range(r):
+        # logger.debug("iteration" + iteration + "=" * 60)
+
+        logger.debug("b_ij:\n%s", b_ij)
+
+        # Compute coupling coefficients
+        # line 4: for all capsule i in layer l: c_i ← softmax(b_i) 
+        c_ij = F.softmax(b_ij, dim=0) # TODO either dim 0 or 1 0 -> 0.33 values, 1 -> 0.5 values
+        c_ij = c_ij.unsqueeze(-1)
+        logger.debug("c_ij:\n%s", c_ij)
+
+        logger.debug("c_ij size: %s", c_ij.size())
+        logger.debug("u_hat size: %s", u_hat.size())
+
+        # Weighted sum of predictions
+        # line 5
+        s_j = torch.sum(c_ij * u_hat, dim=0)  # Weighted sum: [n_higher, d_out]
+        logger.debug("s_j\n%s", s_j.unsqueeze(2))
+
+        # Squash function
+        # line 6
+        v_j = squash(s_j.unsqueeze(2))
+        logger.debug("v_j:\n%s", v_j)
+
+
+        v_j_transformed = v_j.squeeze(2).unsqueeze(0).expand(in_capsule_count, -1, -1)
+
+        # print("u_hat", u_hat)
+        # print("t", t)
+
+        b_ij = b_ij + (u_hat * v_j_transformed).sum(-1) # sum also squeezes matrix by default
+
+    return W, v_j
+    
+if __name__ == "__main__":
+    # main()
+    u = torch.tensor([
+         [0.0, 1.0],
+         [1.0, 1.0],
+         [2.0, 3.0],
+         [1.0, 1.0],
+         [1.0, 1.0],
+    ])
+    procedure1(u, 2, 2)
--- test.py
+++ test.py
@@ -1,90 +1,63 @@
+import logging
+import unittest
+
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
-from demo import W as demo_w
 
-USE_CUDA = True if torch.cuda.is_available() else False
-
-class DigitCaps(nn.Module):
-    def __init__(self, num_capsules=10, num_routes=32 * 6 * 6, in_channels=8, out_channels=16, W=None):
-        super(DigitCaps, self).__init__()
-
-        self.in_channels = in_channels
-        self.num_routes = num_routes
-        self.num_capsules = num_capsules
-
-        # self.W = nn.Parameter(torch.randn(1, num_routes, num_capsules, out_channels, in_channels))
-        self.W = torch.randn(1, num_routes, num_capsules, out_channels, in_channels)
-        print("W in digitcaps:", self.W)
-        # self.W = W # demo_w.unsqueeze(0)
-        # print(demo_w.unsqueeze(0).size())
+from capsnet import DigitCaps
+from dynamic_routing import procedure1
 
 
-    def forward(self, x):
-        safe = x[0]
-        batch_size = x.size(0)
-        x = torch.stack([x] * self.num_capsules, dim=2).unsqueeze(4)
-        print("x size:", x.size())
-        print("x:", x)
+class CapsuleNetworkTests(unittest.TestCase):
 
-        W = torch.cat([self.W] * batch_size, dim=0)
-        print("W:", W)
-        # print("x size:", x.squeeze().size())
-        # print("x:", x)
-        # u = (
-        #     safe
-        #     .unsqueeze(1) # add a new dimension at index 1
-        #     .expand(-1, self.num_capsules, -1)
-        #     .unsqueeze(-1) # add a new dimension after the current last
-        # )
-        # print("u:", u)
-        u_hat = torch.matmul(W, x)
+    def test_similarity(self):
+        torch.manual_seed(127)
 
-        b_ij = torch.zeros(1, self.num_routes, self.num_capsules, 1)
-        # b_ij = Variable(torch.zeros(1, self.num_routes, self.num_capsules, 1))
-        # if USE_CUDA:
-        #     b_ij = b_ij.cuda()
+        u = torch.tensor([[[0.5, 1.0],
+                        [1.0, 1.0],
+                        [1.0, 2.0]]])
 
-        num_iterations = 3
-        for iteration in range(num_iterations):
-            print("Iteration " + str(iteration) + "=" * 60)
-            print("b_ij size", b_ij.size())
-            print("b_ij", b_ij)
-            c_ij = F.softmax(b_ij, dim=1)
-            print("c_ij size", c_ij.size())
-            print("c_ij", c_ij)
+        batch_size, in_capsules, in_dimension = u.size()
+        out_capsules = 2
+        out_dimension = 2
 
-            c_ij = torch.cat([c_ij] * batch_size, dim=0).unsqueeze(4)
-            # print("c_ij", c_ij)
+        # from github
+        capsule = DigitCaps(out_capsules, in_capsules, in_dimension, out_dimension)
 
-            s_j = (c_ij * u_hat).sum(dim=1, keepdim=True)
-            print("s_j:", s_j)
+        res_didigtCaps = capsule.forward(u)
 
-            v_j = self.squash(s_j)
-            print("v_j:", v_j)
+        print("res digitcaps", res_didigtCaps)
+        # print("res2:", res2)
 
-            if iteration < num_iterations - 1:
-                a_ij = torch.matmul(u_hat.transpose(3, 4), torch.cat([v_j] * self.num_routes, dim=1))
-                b_ij = b_ij + a_ij.squeeze(4).mean(dim=0, keepdim=True)
+        # my implementation
+        W, res_custom = procedure1(u.squeeze(0), out_capsules, out_dimension, W=capsule.W.squeeze(0))
 
-        return v_j.squeeze(1)
+        self.assertTrue(torch.allclose(res_didigtCaps, res_custom), "Custom implementation returns different result")
 
-    def squash(self, input_tensor):
-        squared_norm = (input_tensor ** 2).sum(-1, keepdim=True)
-        output_tensor = squared_norm * input_tensor / ((1. + squared_norm) * torch.sqrt(squared_norm))
-        return output_tensor
+    def test_similarity2(self):
+        torch.manual_seed(127)
 
-def main():
-    torch.manual_seed(127)
+        u = torch.tensor([[[0.5, 1.0, 0.1],
+                        [1.0, 1.0, 0.0],
+                        [1.0, 2.0, 3.0],
+                        [2.0, 3.0, 4.0]]])
 
-    test = DigitCaps(2, 3, 2, 2)
-    test_input = torch.tensor([[[0.5, 1],
-                                    [1, 1],
-                                    [0.5, 0.5]]])
+        batch_size, in_capsules, in_dimension = u.size()
+        out_capsules = 6
+        out_dimension = 1
 
-    output = test.forward(test_input)
-    print("from other", output)
+        # from github
+        capsule = DigitCaps(out_capsules, in_capsules, in_dimension, out_dimension)
 
-if __name__ == "__main__":
-    main()
+        res_didigtCaps = capsule.forward(u)
+
+        print("res digitcaps", res_didigtCaps)
+        # print("res2:", res2)
+
+        # my implementation
+        W, res_custom = procedure1(u.squeeze(0), out_capsules, out_dimension, W=capsule.W.squeeze(0))
+
+        self.assertTrue(torch.allclose(res_didigtCaps, res_custom), "Custom implementation returns different result")
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
