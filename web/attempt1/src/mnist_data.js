ddeae0ec874f64030ecff0c9a3ecf418c980349c
Zsolt Kébel <25386639+zsoltkebel@users.noreply.github.com>
2025-03-13 22:18:02 +0000

    Use my github hosted mnist images and labels

--- web/attempt1/src/mnist_data.js
+++ web/attempt1/src/mnist_data.js
@@ -1,8 +1,8 @@
 import * as tf from "@tensorflow/tfjs"
 
 
-const MNIST_IMAGES_SPRITE_PATH = 'https://storage.googleapis.com/learnjs-data/model-builder/mnist_images.png';
-const MNIST_LABELS_PATH = 'https://storage.googleapis.com/learnjs-data/model-builder/mnist_labels_uint8';
+const MNIST_IMAGES_SPRITE_PATH = 'https://raw.githubusercontent.com/zsoltkebel/capsnet-models/main/mnist-dataset/mnist_images.png';
+const MNIST_LABELS_PATH = 'https://raw.githubusercontent.com/zsoltkebel/capsnet-models/main/mnist-dataset/mnist_labels_uint8';
 
 
 export class MNISTData {
